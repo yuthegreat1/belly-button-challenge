10a982771a45f6f418839c0909acb71881b1b94d
Brandon Yu <brandyu@bu.edu>
2022-11-30 15:05:23 -0500

    added bubble graph

---
+++ static/js/app.js
@@ -0,0 +1,77 @@
+const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
+
+function init() {
+    var dropDown = d3.select("#selDataset");
+    // Fetch the JSON data
+    d3.json(url).then(function (data) {
+        var sampleId = data.names;
+        sampleId.forEach((sample) => {
+            dropDown.append("option").text(sample).property("value", sample)
+        });
+        var initSample = sampleId[0];
+        buildDemo(initSample);
+        buildCharts(initSample);
+        // console.log(data);
+    });
+};
+
+function buildCharts(sample) {
+    d3.json(url).then(function (data) {
+        // console.log(data.samples)
+        var allSamples = data.samples;
+        var sampleInfo = allSamples.filter(row => row.id == sample);
+        var sampleValues = sampleInfo[0].sample_values;
+        var sampleValuesSlice = sampleValues.slice(0,10).reverse();
+        var otuIds = sampleInfo[0].otu_ids;
+        var otuIdsSlice = otuIds.slice(0,10).reverse();
+        var otuLabels = sampleInfo[0].otu_labels;
+        var otuLabelsSlice = otuLabels.slice(0,10).reverse();
+        var metaData = data.metadata;
+        var metaDataSample = metaData.filter(row => row.id == sample);
+        var wash = metaDataSample[0].wfreq;
+
+        // console.log(metaData);
+
+        var trace1 = {
+            x: sampleValuesSlice,
+            y: otuIdsSlice.map(item => `OTU ${item}`),
+            type: "bar",
+            orientation: "h",
+            text: otuLabelsSlice,
+        };
+        var data = [trace1];
+        Plotly.newPlot("bar", data)
+        var trace2 = {
+            x: otuIds,
+            y: sampleValues,
+            mode: "markers",
+            marker: {
+                size: sampleValues,
+                color: otuIds,
+                colorscale: "Earth"
+            },
+            text: otuIds
+        };
+        var bubble_data = [trace2];
+        var layout = {
+            showlegend: false
+        };
+
+        Plotly.newPlot("bubble", bubble_data, layout);
+    });
+};
+
+function buildDemo(sample) {
+    var demo = d3.select("#sample-metadata");
+    d3.json(url).then(function (data) {
+        var metaData = data.metadata;
+        var metaDataSample = metaData.filter(row => row.id == sample);
+        demo.selectAll("p").remove();
+        metaDataSample.forEach((row) => {
+            for (const [key, value] of Object.entries(row)) {
+                demo.append("p").text(`${key}: ${value}`);
+            };
+        });
+    });
+};
+init();
